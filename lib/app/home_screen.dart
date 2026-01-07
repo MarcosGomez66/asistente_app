4de7dd1bd5f9ec470c8d6cf815bc006004e04c97
Marcos <marcosdgomez66@gmail.com>
2026-01-07 17:23:21 -0300

    open and close button has improved

--- lib/app/home_screen.dart
+++ lib/app/home_screen.dart
@@ -11,16 +11,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool activeWDay = false;
 
-  void toggleWDay() {
+  void DwInit() {
     setState(() {
-      activeWDay = !activeWDay;
+      activeWDay = true;
     });
+    // guardar fecha de inicio en backend
+  }
+
+  void DwEnded() {
+    setState(() {
+      activeWDay = false;
+    });
+    // guardad fecha de cierre en backen
+  }
+
+  Future<bool> DwEndConfirm(BuildContext context) async {
+    return await showDialog<bool>(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text('Terminar jornada'),
+              content: const Text(
+                "Desea Finalizar la jornada? \n Esta acción guardará los datos del día",
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context, false),
+                  child: const Text('Cancelar'),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () => Navigator.pop(context, true),
+                  child: const Text('Terminar'),
+                ),
+              ],
+            );
+          },
+        ) ??
+        false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Asistente de ventas', style: titleStyle), backgroundColor: Colors.deepPurple,),
+      appBar: AppBar(
+        title: Text('Asistente de ventas', style: titleStyle),
+        backgroundColor: Colors.deepPurple,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -32,7 +70,15 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: activeWDay ? Colors.green : Colors.red,
                 ),
-                onPressed: toggleWDay,
+                onPressed: () async {
+                  if (activeWDay) {
+                    final confirm = await DwEndConfirm(context);
+                    if (!confirm) return;
+                    DwEnded();
+                  } else {
+                    DwInit();
+                  }
+                },
                 child: Text(
                   activeWDay ? 'Abierto' : 'Cerrado',
                   style: const TextStyle(fontSize: 18, color: Colors.white),
